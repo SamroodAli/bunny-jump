8ec77d802eb2e2e84bf20b500e1ac7d5ed386381
samroodAli <devsamrood@gmail.com>
2021-07-31 18:54:19 +0530

    Game:Add physics property to game scene with debug:true to see collision boxes

--- src/index.js
+++ src/index.js
@@ -7,4 +7,13 @@ export default new Phaser.Game({
   width: 480,
   height: 640,
   scene: Game,
+  physics: {
+    default: "arcade",
+    arcade: {
+      gravity: {
+        y: 200,
+      },
+      debug: true,
+    },
+  },
 });
