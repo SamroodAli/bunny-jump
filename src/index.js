09fee8f3d6c7b7aa1cab2942ae892a104c51a373
samroodAli <devsamrood@gmail.com>
2021-07-31 13:41:10 +0530

    Config:Setup scenes folder for putting game scenes

--- src/index.js
+++ src/index.js
@@ -2,4 +2,8 @@ import "./style.scss";
 console.log("Happy gaming");
 import Phaser from "phaser";
 
-console.log(Phaser);
+export default new Phaser.Game({
+  type: Phaser.AUTO,
+  width: 400,
+  height: 640,
+});
